ed9d12a7d4b292b3146ed68024ce0716994c050c
gianluca cerritelli <gianluca.cerritelli@protolabs.co.uk>
2015-08-19 14:53:34 +0100

    Angular grades directive

--- Ductia/Ductia.Web/AngularUI/ductia.js
+++ Ductia/Ductia.Web/AngularUI/ductia.js
@@ -1,29 +1,40 @@
-﻿var ductia = angular.module("ductia", ['ngAnimate', 'ui.bootstrap']);
+﻿(function() {
+	'use strict';
 
-ductia.controller("SearchController", ["$scope", "$http",
-	function ($scope, $http) {
-		$scope.searchResult = "";
+	angular.module('ductia',
+		['ngAnimate', 'ui.bootstrap'])
+		.controller("SearchController", [
+			"$scope", "$http",
+			function($scope, $http) {
+				$scope.grades = [1, 2, 3, 4, 5, 7, 8];
 
-		var searchServiceUrl = "http://localhost:2487/";
-		var restfulSearchBookByPieceTitle = "search/books/{pieceName}";
+				$scope.searchResult = "";
+
+				var searchServiceUrl = "http://localhost:2487/";
+				var restfulSearchBookByPieceTitle = "search/books/{pieceName}";
 
 
-		$scope.searchByName = function (name) {
+				$scope.searchByName = function(name) {
 
-			var requestedSearch = searchServiceUrl + restfulSearchBookByPieceTitle.replace("{pieceName}", name);
-			$http.get(requestedSearch)
-				.success(function (data) {
-					$scope.searchResult = data;
-				})
-				.error(function (data, status, headers, config) {
-					$scope.searchResult = config;
+					var requestedSearch = searchServiceUrl + restfulSearchBookByPieceTitle.replace("{pieceName}", name);
+					$http.get(requestedSearch)
+						.success(function(data) {
+							$scope.searchResult = data;
+						})
+						.error(function(data, status, headers, config) {
+								$scope.searchResult = config;
 
-					alert("error!");
-					//alert("status: " + status);
-					//alert("headers: " + headers);
-					//alert("config: " + config);
-					//alert(data);
+								alert("error!");
+							}
+						);
 				}
-				);
-		}
-	}]);
+			}
+		])
+		.directive('grades', function() {
+				return function(scope, el, attrs) {
+						el.bind('mousedown', function(e) {
+							e.metaKey = true;
+						}).selectable();
+				}
+			});
+})();
