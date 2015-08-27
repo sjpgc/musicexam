de1628105696c121de88134765991d5bc9627914
gianluca cerritelli <gianluca.cerritelli@protolabs.co.uk>
2015-08-27 10:17:32 +0100

    Added search service

--- Ductia/Ductia.Web/AngularUI/ductia.js
+++ Ductia/Ductia.Web/AngularUI/ductia.js
@@ -3,40 +3,73 @@
 
 	angular.module('ductia',
 		['ngAnimate', 'ui.bootstrap'])
+		.factory("searchService", function ($http) {
+			function processResult(response) {
+				return response.data;
+			}
+
+			var searchServiceUrl = "http://localhost:2487/";
+			var restfulSearchBookByPieceTitle = "search/books/{pieceName}";
+			var fac = {};
+			fac.searchByName = function (name, callbackSuccess, callbackError) {
+				var request = $http({
+					method: "GET",
+					url: searchServiceUrl + restfulSearchBookByPieceTitle.replace("{pieceName}", name)
+				});
+				return request.then(function(data) {
+					var processedData = processResult(data);
+					if (typeof callbackSuccess !== "undefined") {
+						callbackSuccess(processedData);
+					}
+				}, function() {
+					if (typeof callbackError !== "undefined") {
+						callbackError("There was an error retrieving your data");
+					}
+				});
+			}
+			fac.searchByCriteria = function(eboard, instrument, grades) {
+				
+			}
+
+			return fac;
+		})
 		.controller("SearchController", [
-			"$scope", "$http",
-			function($scope, $http) {
+			"$scope", "searchService",
+			function($scope, searchService) {
 				$scope.examBoards = ["ABRSM", "Trinity"];
 				$scope.instruments = ["Flute", "Descant Recorder", "Alto Recorder", "Guitar"];
 				$scope.grades = [1, 2, 3, 4, 5, 6, 7, 8];
-
-
+				$scope.selectedGrades = [];
 				$scope.searchResult = "";
-
-				var searchServiceUrl = "http://localhost:2487/";
-				var restfulSearchBookByPieceTitle = "search/books/{pieceName}";
-
-
 				$scope.searchByName = function(name) {
-
-					var requestedSearch = searchServiceUrl + restfulSearchBookByPieceTitle.replace("{pieceName}", name);
-					$http.get(requestedSearch)
-						.success(function(data) {
+					searchService.searchByName(name, function(data) {
 							$scope.searchResult = data;
-						})
-						.error(function(data, status, headers, config) {
-								$scope.searchResult = config;
-								alert("error!");
-							}
-						);
-				}
+						},
+						function(errorMessage) {
+							alert(errorMessage);
+						});
+				};
+				$scope.searchByCriteria = function (eboard, instrument, selgrades) {
+					$scope.selectedGrades = $($scope.selectedGrades).sort();
+					alert(JSON.stringify($scope.selectedGrades));
+				};
 			}
 		])
 		.directive('ductiaGrades', function() {
 				return function(scope, el, attrs) {
 					el.bind('mousedown', function (e) {
 						e.metaKey = true;
-					}).selectable();
+					}).selectable({
+						selected: function(event, ui) {
+							scope.selectedGrades.push($(ui.selected).text().trim());
+						},
+						unselected: function(event, ui) {
+							var unselectedValue = $(ui.unselected).text().trim();
+							scope.selectedGrades = $.grep(scope.selectedGrades, function(value) {
+								return value.trim() !== unselectedValue;
+							});
+						}
+					});
 				}
 			});
 })();
